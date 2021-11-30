716cb9c9c073aa1ffb6e59b8baca4fe94698f0be
Eimantas Sakavicius <eimantas.sakavicius@gmail.com>
2021-11-30 10:30:17 +0200

    Add project files.

---
+++ VirtualPetSimulator/VirtualPet.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VirtualPetSimulator
+{
+    class VirtualPet
+    {
+        public string FullName { get; set; }
+        public int Age { get; set; }
+        public string Speciecs { get; set; }
+        public bool IsAwake { get; set; }
+        private int ExpierencePoints { get; set; }
+
+        public VirtualPet(string petName, int petAge, string petSpecies, bool petIsAwake)
+        {
+            FullName = petName;
+            Age = petAge;
+            Speciecs = petSpecies;
+            IsAwake = petIsAwake;
+        }
+
+        public void Greet()
+        {
+            Console.WriteLine($"May name is {FullName}, the {Speciecs}");
+            Console.WriteLine($"I am {Age} years old");
+            Console.WriteLine($"Is awake? {IsAwake}");
+        }
+
+        public void Sleep()
+        {
+            IsAwake = false;
+            Console.WriteLine($"{FullName} is now happily snoring... Zzzz");
+        }
+
+        public void Eat(string foodName)
+        {
+            Console.WriteLine($"{FullName} is eating now {foodName}");
+        }
+    }
+}
---
+++ VirtualPetSimulator/World.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VirtualPetSimulator
+{
+    class World
+    {
+        public void Run()
+        {
+            Console.Title = "=== Virtual Pet Simulator ===";
+            Console.WriteLine(@" __      ___      _               _   _____     _   
+ \ \    / (_)    | |             | | |  __ \   | |  
+  \ \  / / _ _ __| |_ _   _  __ _| | | |__) |__| |_ 
+   \ \/ / | | '__| __| | | |/ _` | | |  ___/ _ \ __|
+    \  /  | | |  | |_| |_| | (_| | | | |  |  __/ |_ 
+     \/   |_|_|   \__|\__,_|\__,_|_| |_|   \___|\__|");
+            Console.WriteLine("Welcome to pet simulator!");
+            Console.WriteLine("");
+
+            VirtualPet leo = new VirtualPet("Leo", 12, "Cat", true);
+            //leo.ExpierencePoints = 10;
+            Console.WriteLine("> Pet1");
+            leo.Greet();
+            leo.Eat("some dry food");
+            Console.WriteLine("");
+
+            VirtualPet junior = new VirtualPet("Junior", 50, "Parrot", false);
+            Console.WriteLine("> Pet2");
+            junior.Greet();
+            junior.Eat("a worm");
+            junior.Sleep();
+            Console.WriteLine("");
+
+            VirtualPet callieTheUnicorn = new VirtualPet("Callie", 250, "Unicorn", true);
+            Console.WriteLine("> Pet3");
+            callieTheUnicorn.Greet();
+            callieTheUnicorn.Eat("rainbows");
+            Console.WriteLine("");
+
+            Console.WriteLine("Press any key to exit...");
+        }
+    }
+}
